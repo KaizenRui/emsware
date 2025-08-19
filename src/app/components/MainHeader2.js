36c33b16058ab227bd0a1d22c12e18a7ab7418f8
Regienald Cabaña Montalvo <sync.regie.cm@gmail.com>
2025-08-19 20:55:47 +0800

    ORBITRON BORN

--- src/app/components/MainHeader2.js
+++ src/app/components/MainHeader2.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
-import { Orbitron } from 'next/font/google';
+import { Orbitron } from "next/font/google";
+
+const orbitron = Orbitron({
+    subsets: ["latin"],
+    weight: "400", // you can change to "700" etc.
+});
 
 const MainHeader2 = () => {
     return (
-        <div className="flex h-[50px] w-screen border">
+        <div className="flex h-[20px] w-screen border">
             <div className="relative w-[7.5%] bg-gray-200">
                 <Image
                     src="/EMSWARE.png"
@@ -12,8 +17,10 @@ const MainHeader2 = () => {
                     className="object-fill"
                 />
             </div>
-            <div className="flex-1 flex items-center justify-center bg-white text-xl font-bold text-black font-orbitron">
-                ORBITRON TEXT
+            <div
+                className={`flex-1 flex items-center justify-center bg-white text-xl font-bold text-header text-black ${orbitron.className}`}
+            >
+            TESTHEADER
             </div>
 
         </div>
